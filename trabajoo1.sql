a57120ccf7d76cf3cb8994bf5037050c110e1c44
luisa <ramosluisa858@gmail.com>
2025-05-13 13:11:05 -0500

    tabla creada

---
+++ trabajoo1.sql
@@ -0,0 +1,24 @@
+INSERT INTO Fabricantes (nombre) 
+VALUES ("Asus"),
+("Lenovo"),
+("Hewlett-Packard"),
+("Samsung"),
+("Seagate"),
+("Crucial"),
+("Gigabyte"),
+("Huawei"),
+("Xiaomi");      
+
+
+INSERT INTO Productos (nombre,Precio) 
+VALUES ("Disco duro SATA3 1TB",86.99),
+("Memoria RAM DDR4 8GB",120),
+("Disco SSD 1 TB",150.99),
+("GeForce GTX 1050Ti",185),
+("GeForce GTX 1080 Xtreme",755),
+("Monitor 24 LED Full HD",202),
+("Monitor 27 LED Full HD",245.99),
+("Portatil Yoga 520",559),
+("Portatil Ideapd 320",444),
+("Impresora HP Dskjet 3720",59.99),
+("Impresora HP Laserjet Pro M26nw",180);
